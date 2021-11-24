df7c8421df802894531b9270fb116bf9973cff64
George <yura_lex@mail.ru>
2021-11-24 18:14:15 +0300

    Export to excel works

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,13 @@
 namespace LatinScrapper
 {
+    using OfficeOpenXml;
     using PuppeteerSharp;
     public class LatinData
     {
-        public string Word;
-        public string Meaning;
+        public string Word { get; set; }
+        public string Conjuration { get; set; }
+        public string Meaning { get; set; }
+        public string Analysis { get; set; }
     }
 
     public partial class Form1 : Form
@@ -12,7 +15,7 @@ namespace LatinScrapper
         private readonly string siteUrl = "https://www.dl.cambridgescp.com/Array/book-ii-stage-stage-teachers-guide";
         private readonly string exportPath;
         private Browser _browser;
-        private Page _page;
+        private PuppeteerSharp.Page _page;
         public Form1()
         {
             InitializeComponent();
@@ -29,12 +32,7 @@ namespace LatinScrapper
             await _page.GoToAsync(siteUrl);
            
         }
-        // TODO get conjuration and meaning of the current word
-        //var getWords = @"const words = [...document.querySelectorAll('span')].filter(e => {
-        //      return e.id.match(/w\d+/gm)
-        //})";
-
-        //let word = document.querySelector('span#w0.w').innerText
+ 
         private async void button2_Click(object sender, EventArgs e)
         {
             var pages = await _browser.PagesAsync();
@@ -46,22 +44,32 @@ namespace LatinScrapper
                 try
                 {
                     var wordsCount = (int)await page.EvaluateFunctionAsync(@"() => [...document.querySelectorAll('span')].filter(e => e.id.match(/w\d+/gm)).length");
-                    await page.ClickAsync($"span#anay3.loli");
                     await page.ClickAsync($"span#w{wordIndex}");
                     while (wordIndex < wordsCount)
                     {
                         var word = await page.WaitForSelectorAsync($"span#w{wordIndex++}");
                         var wordText = (string)await word.EvaluateFunctionAsync("el => el.textContent");
                         var meaning = await page.WaitForSelectorAsync($"div#prs");
-                        var meaningText = (string)await meaning.EvaluateFunctionAsync("el => el.textContent");
+                        var conjText = (string)await meaning.EvaluateFunctionAsync("el => el.firstElementChild.textContent");
+                        var analysisText = (string)await meaning.EvaluateFunctionAsync("el => el.lastElementChild.textContent");
+                        var meaningText = (string)await meaning.EvaluateFunctionAsync(@"el =>{
+                            const c = el.firstElementChild.textContent;
+                            const g = el.lastElementChild.textContent;
+                            return el.textContent.split(c)[1].split(g)[0].trim();
+                            }");
+
                         data.Add(new LatinData
                         {
                             Word = wordText,
+                            Conjuration = conjText,
                             Meaning = meaningText,
+                            Analysis = analysisText,
                         });
                         var keyboard = page.Keyboard;
                         await keyboard.PressAsync("ArrowRight");
                     }
+                    ExportToExcel(data, @"C:\projects\test1.xlsx");
+
                 }
                 finally
                 {
@@ -71,6 +79,33 @@ namespace LatinScrapper
                 
             }
             
+        }
+
+        private void ExportToExcel(List<LatinData> latinData, string path)
+        {
+            var outputStream = new MemoryStream();
+            try
+            {
+                using(var package = new ExcelPackage(outputStream))
+                {
+                    var ws = package.Workbook.Worksheets.Add("Vocabulary");
+                    var dataRange = ws.Cells["A1"].LoadFromCollection(latinData, true);
+                    ws.Column(1).AutoFit();
+                    ws.Column(2).AutoFit();
+                    ws.Column(3).AutoFit();
+                    ws.Column(4).AutoFit();
+                    package.Save();
+                    outputStream.Position = 0;
+
+                    byte[] data = package.GetAsByteArray();
+                    File.WriteAllBytes(path, data);
+                }
+            }
+            catch
+            {
+                outputStream.Dispose();
+                return;
+            }
         }
     }
 }
