03753f599fdc932765637cd3b0930147715ae0b1
George <yura_lex@mail.ru>
2021-11-25 10:30:55 +0300

    Final version of scrapper algorithm

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 namespace LatinScrapper
 {
+    using Newtonsoft.Json;
     using OfficeOpenXml;
     using PuppeteerSharp;
     public class LatinData
@@ -15,7 +16,7 @@ namespace LatinScrapper
         private readonly string siteUrl = "https://www.dl.cambridgescp.com/Array/book-ii-stage-stage-teachers-guide";
         private readonly string exportPath;
         private Browser _browser;
-        private PuppeteerSharp.Page _page;
+        private Page _page;
         public Form1()
         {
             InitializeComponent();
@@ -29,10 +30,11 @@ namespace LatinScrapper
                 ExecutablePath = $"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
             });
             _page = await _browser.NewPageAsync();
+            await _page.SetViewportAsync(new ViewPortOptions { Width = 1200, Height = 800 });
             await _page.GoToAsync(siteUrl);
            
         }
- 
+        
         private async void button2_Click(object sender, EventArgs e)
         {
             var pages = await _browser.PagesAsync();
@@ -43,6 +45,8 @@ namespace LatinScrapper
                 var wordIndex = 0;
                 try
                 {
+                    var documentNameNode = await page.WaitForSelectorAsync($"div#hdr");
+                    var documentName = (string)await documentNameNode.EvaluateFunctionAsync("el => el.firstElementChild.previousSibling.nodeValue");
                     var wordsCount = (int)await page.EvaluateFunctionAsync(@"() => [...document.querySelectorAll('span')].filter(e => e.id.match(/w\d+/gm)).length");
                     await page.ClickAsync($"span#w{wordIndex}");
                     while (wordIndex < wordsCount)
@@ -50,38 +54,66 @@ namespace LatinScrapper
                         var word = await page.WaitForSelectorAsync($"span#w{wordIndex++}");
                         var wordText = (string)await word.EvaluateFunctionAsync("el => el.textContent");
                         var meaning = await page.WaitForSelectorAsync($"div#prs");
-                        var conjText = (string)await meaning.EvaluateFunctionAsync("el => el.firstElementChild.textContent");
-                        var analysisText = (string)await meaning.EvaluateFunctionAsync("el => el.lastElementChild.textContent");
-                        var meaningText = (string)await meaning.EvaluateFunctionAsync(@"el =>{
-                            const c = el.firstElementChild.textContent;
-                            const g = el.lastElementChild.textContent;
-                            return el.textContent.split(c)[1].split(g)[0].trim();
+                        var scrappedData = await meaning.EvaluateFunctionAsync(@"el =>{
+                                let res
+                                switch (el.children.length) {
+                                    case 1 : {
+                                        let c = el.firstElementChild.textContent;
+                                        let grammar =  el.firstElementChild.nextSibling.nodeValue.trim();
+                                        res = [{Conjuration: c, Meaning: grammar}]
+                                        break;
+                                    }
+                                    case 2 : {
+                                        let c = el.firstElementChild.textContent;
+                                        let grammar =  el.firstElementChild.nextSibling.nodeValue.trim();
+                                        let g = el.lastElementChild.textContent;
+                                        res = [{Conjuration: c, Meaning: grammar, Analysis: g}]
+                                        break;
+                                    }
+                                    case 4 : {
+                                       let w1 = el.firstElementChild.textContent;
+                                       let m1 = el.firstElementChild.nextSibling.nodeValue.trim();
+                                       let w2 = el.children[2].textContent;
+                                       let m2 = el.children[2].nextSibling.nodeValue.trim();
+                                       let analys = el.lastElementChild.textContent;
+                                       res = [{Conjuration: w1, Meaning: m1, Analysis: analys},
+                                              {Word: w2, Meaning: m2}]
+                                       break;
+                                    }
+                                    default : {
+                                        res = [];
+                                    }
+                                }
+                                return res;
                             }");
-
-                        data.Add(new LatinData
-                        {
-                            Word = wordText,
-                            Conjuration = conjText,
-                            Meaning = meaningText,
-                            Analysis = analysisText,
+                        var scrappedDataString = JsonConvert.SerializeObject(scrappedData);
+                        var result = JsonConvert.DeserializeObject<List<LatinData>>(scrappedDataString);
+                        result.ForEach(x => { 
+                            if (x.Word == null)
+                            {
+                                x.Word = wordText;
+                            }
                         });
+                        data.AddRange(result);
                         var keyboard = page.Keyboard;
                         await keyboard.PressAsync("ArrowRight");
                     }
-                    ExportToExcel(data, @"C:\projects\test1.xlsx");
+                    var distinctData = data.DistinctBy(x => x.Word).OrderBy(x => x.Word);
+                    ExportToExcel(distinctData, @$"C:\projects\{documentName}.xlsx");
 
                 }
                 finally
                 {
                     await _browser.CloseAsync();
                     _browser?.Dispose();
+                    _browser = null;
                 }
-                
+
             }
             
         }
 
-        private void ExportToExcel(List<LatinData> latinData, string path)
+        private void ExportToExcel(IEnumerable<LatinData> latinData, string path)
         {
             var outputStream = new MemoryStream();
             try
@@ -89,7 +121,7 @@ namespace LatinScrapper
                 using(var package = new ExcelPackage(outputStream))
                 {
                     var ws = package.Workbook.Worksheets.Add("Vocabulary");
-                    var dataRange = ws.Cells["A1"].LoadFromCollection(latinData, true);
+                    var dataRange = ws.Cells["A1"].LoadFromCollection(latinData, true, OfficeOpenXml.Table.TableStyles.Medium1);
                     ws.Column(1).AutoFit();
                     ws.Column(2).AutoFit();
                     ws.Column(3).AutoFit();
